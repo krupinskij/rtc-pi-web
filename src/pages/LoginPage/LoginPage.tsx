05b265e48ae7b421c30d367137badd83f013f5c8
Jan Krupiński <jk.krupinski@gmail.com>
2021-11-29 22:42:11 +0100

    Login View: Add some content

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,5 +1,36 @@
+import { Theme, Typography } from '@mui/material';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import { styled } from '@mui/system';
+
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <PageWrapper>
+      <FormWrapper>
+        <CardContent>
+          <Typography gutterBottom variant="h4" component="h2">
+            Zaloguj się
+          </Typography>
+          <Typography variant="body2">Zaloguj się</Typography>
+        </CardContent>
+      </FormWrapper>
+    </PageWrapper>
+  );
 };
 
 export default LoginPage;
+
+const PageWrapper = styled('main')<{ theme?: Theme }>(
+  ({ theme }: { theme: Theme }) => `
+    margin: ${theme.spacing(8, 4)};
+  `
+);
+
+const FormWrapper = styled(Card)<{ theme?: Theme }>(
+  ({ theme }: { theme: Theme }) => `
+  
+    margin: ${theme.spacing(0, 80)};
+    color: white;
+    background-color: ${theme.palette.grey[800]};
+  `
+);
