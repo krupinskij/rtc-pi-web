0b79e5632610b9369547bea346dfc9de610143f9
Jan Krupiński <jk.krupinski@gmail.com>
2021-11-30 22:51:50 +0100

    Login View: Update navbar

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -1,13 +1,15 @@
 import { Theme, Toolbar, Typography } from '@mui/material';
 import AppBar from '@mui/material/AppBar';
 import { styled } from '@mui/system';
+import ScreenSearchDesktopIcon from '@mui/icons-material/ScreenSearchDesktop';
 
 const Navbar = () => {
   return (
     <NavbarWrapper position="static">
       <Toolbar>
-        <Typography variant="h6" component="div">
-          RaspRTC
+        <LogoIcon fontSize="large" />
+        <Typography variant="h5" component="div">
+          RTC<LogoSpan>Pi</LogoSpan>
         </Typography>
       </Toolbar>
     </NavbarWrapper>
@@ -21,5 +23,18 @@ const NavbarWrapper = styled(AppBar)<{ theme?: Theme }>(
     height: ${theme.spacing(16)};
 
     background-color: ${theme.palette.background.paper};
+    color: ${theme.palette.text.primary}
+  `
+);
+
+const LogoSpan = styled('span')<{ theme?: Theme }>(
+  ({ theme }: { theme: Theme }) => `
+    color: ${theme.palette.primary.main}
+  `
+);
+
+const LogoIcon = styled(ScreenSearchDesktopIcon)<{ theme?: Theme }>(
+  ({ theme }: { theme: Theme }) => `
+    margin-right: ${theme.spacing(2)}
   `
 );
